87a94c506738a8f1085412473e3081f814f4f9eb
LijKG <kirubelgirma027@gmail.com>
2022-07-07 19:34:40 -0700

    leap year

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
+* Description: avoid infinite loop
 * Return: 0
 */
+
 int main(void)
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
-
+i = 0;
 /**
 * while (i < 10) - less than zero
 * {
 * putchar(i); - print i
-* }
+*}
 */
-        printf("Infinite loop avoided! \\o/\n");
+pintf("Infinite loop avoided! \\o/\n");
 
-	return (0);
-
+return (0);
 }
