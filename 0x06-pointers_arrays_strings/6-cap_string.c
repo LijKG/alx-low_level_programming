01e3ed89d8e5dfab0bb3b52e9d6241ca241465ca
LijKG <kirubelgirma027@gmail.com>
2022-07-15 16:41:07 -0700

    #include main.h

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,42 +1,27 @@
 #include "main.h"
 /**
-* _strlen - returns the length of a string
-* @s: string
-* Return: returns length as integer;
+* cap_string - capitalize first letter of each word
+* @s: string to manipulate
+* Return: string
 */
-int _strlen(char *s)
+char *cap_string(char *s)
 {
-int len = 0;
-while (*(s + len) != '\0')
-len++;
-return (len);
+int i = 0;
+/* check first index for capital */
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 'a' + 'A';
+i++;
+while (s[i] != '\0') /* iterate through string */
+{
+/* if lowercase and prior char is separator, capitalize*/
+if ((s[i] >= 'a' && s[i] <= 'z')
+&& (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+|| s[i - 1] == '\n'))
+s[i] = s[i] - 'a' + 'A';
+i++;
 }
-/**
-* cap_string - function that capitalize first character of a word
-* @str: string to capitalize
-* Return: returns the capitalized string
-*/
-char *cap_string(char *str)
-{
-int index = 0;
-while (str[++index])
-{
-while (!(str[index] >= 'a') && (str[index] <= 'z'))
-index++;
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}')
-str[index] -= 32;
-}
-return (str);
+return (s);
 }
