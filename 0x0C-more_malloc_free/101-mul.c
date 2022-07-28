e61eb7b8b06530e201eb0659d39a0e19d41e8c1c
LijKG <kirubelgirma027@gmail.com>
2022-07-28 16:43:46 -0700

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,7 +20,6 @@ int len = 0;
 
 while (*str++)
 len++;
-
 return (len);
 }
 
@@ -37,17 +36,12 @@ char *create_xarray(int size)
 {
 char *array;
 int index;
-
 array = malloc(sizeof(char) * size);
-
 if (array == NULL)
 exit(98);
-
 for (index = 0; index < (size - 1); index++)
 array[index] = 'x';
-
 array[index] = '\0';
-
 return (array);
 }
 
@@ -82,7 +76,6 @@ if (digit < 0 || digit > 9)
 printf("Error\n");
 exit(98);
 }
-
 return (digit);
 }
 
@@ -102,13 +95,11 @@ int mult_len, num, tens = 0;
 
 mult_len = find_len(mult) - 1;
 mult += mult_len;
-
 while (*prod)
 {
 *prod = 'x';
 prod++;
 }
-
 prod--;
 
 while (zeroes--)
@@ -116,7 +107,6 @@ while (zeroes--)
 *prod = '0';
 prod--;
 }
-
 for (; mult_len >= 0; mult_len--, mult--, prod--)
 {
 if (*mult < '0' || *mult > '9')
@@ -124,13 +114,11 @@ if (*mult < '0' || *mult > '9')
 printf("Error\n");
 exit(98);
 }
-
 num = (*mult - '0') * digit;
 num += tens;
 *prod = (num % 10) + '0';
 tens = num / 10;
 }
-
 if (tens)
 *prod = (tens % 10) + '0';
 }
@@ -148,17 +136,14 @@ int num, tens = 0;
 
 while (*(final_prod + 1))
 final_prod++;
-
 while (*(next_prod + 1))
 next_prod++;
-
 for (; *final_prod != 'x'; final_prod--)
 {
 num = (*final_prod - '0') + (*next_prod - '0');
 num += tens;
 *final_prod = (num % 10) + '0';
 tens = num / 10;
-
 next_prod--;
 next_len--;
 }
@@ -171,7 +156,6 @@ tens = num / 10;
 final_prod--;
 next_prod--;
 }
-
 if (tens)
 *final_prod = (tens % 10) + '0';
 }
@@ -181,12 +165,12 @@ if (tens)
 * @argv: The number of arguments passed to the program.
 * @argc: An array of pointers to the arguments.
 * Description: If the number of arguments is incorrect or one number
-*
 *              contains non-digits, the function exits with a status of 98.
 * Return: Always 0.
 */
 
 int main(int argc, char *argv[])
+
 {
 char *final_prod, *next_prod;
 int size, index, digit, zeroes = 0;
@@ -196,7 +180,6 @@ if (argc != 3)
 printf("Error\n");
 exit(98);
 }
-
 if (*(argv[1]) == '0')
 argv[1] = iterate_zeroes(argv[1]);
 if (*(argv[2]) == '0')
@@ -206,11 +189,9 @@ if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
 printf("0\n");
 return (0);
 }
-
 size = find_len(argv[1]) + find_len(argv[2]);
 final_prod = create_xarray(size + 1);
 next_prod = create_xarray(size + 1);
-
 for (index = find_len(argv[2]) - 1; index >= 0; index--)
 {
 digit = get_digit(*(argv[2] + index));
@@ -223,9 +204,7 @@ if (final_prod[index] != 'x')
 putchar(final_prod[index]);
 }
 putchar('\n');
-
 free(next_prod);
 free(final_prod);
-
 return (0);
 }
